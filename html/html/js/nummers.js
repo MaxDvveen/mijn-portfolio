0bf55f2bed9c1832aca532085559b20a6d91e164
MaxDvveen <145124886+MaxDvveen@users.noreply.github.com>
2023-09-15 14:25:15 +0200

    Add files via upload

---
+++ html/html/js/nummers.js
@@ -0,0 +1,6 @@
+script
+
+var nummers = [1, 4, 3, 12, 123];
+console.log("Lengte van nummers: " + nummers.length);
+console.log("Eerste element: " + nummers[0]);
+console.log("Laatste element: " + nummers[nummers.length - 1]);
